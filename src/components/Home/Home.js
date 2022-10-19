fff81108c9f09c9644c237a7fc12dcdd61e952ff
IsabelNzioka <isabelnzioka@gmail.com>
2022-10-20 02:36:39 +0300

    footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,11 @@ import chef4 from "../../assets/img/chef/chef5.jpg";
 import chef5 from "../../assets/img/chef/chef6.jpg";
 import chef6 from "../../assets/img/chef/chef7.jpg";
 
+import restaurantPicture1 from "../../assets/img/rest17.jpg";
+import restaurantPicture2 from "../../assets/img/rest16.jpg";
+
 import { FaYelp } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const images = [chef1, chef2, chef3];
@@ -47,13 +51,45 @@ const Home = () => {
         <div className="Restaurant">
           <div className="One">
             <div className="OneOne">
-              <h2 className="Teamm">Restaurant</h2>
+              <h2 className="RestaurantName">
+                La Belle <span>Restaurant</span>
+              </h2>
             </div>
-            <div className="OneTwo">hi</div>
+            <div className="OneTwo">
+              <p className="RestaurantDesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum varius nisl nec purus luctus dictum. Cras dapibus
+                metus eu lacinia commodo. Aenean vehicula erat turpis.
+                Suspendisse sed imperdiet leo. Vestibulum consequat blandit
+                euismod. Aenean at lacinia libero, in molestie mi. Maecenas ac
+                turpis. Curabitur a tincidunt tortor. Cras at dui pellentesque,
+                varius sapien id, porta mauris. Sed tempus dui ut justo aliquam,
+                vel
+              </p>
+
+              <button className="RestaurantButton">
+                More <FaArrowRight />{" "}
+              </button>
+            </div>
           </div>
 
-          <div className="Two">two</div>
-          <div className="Three">three</div>
+          <div className="Two">
+            <img
+              src={restaurantPicture1}
+              alt="restaurantPicture"
+              width="100%"
+              height="100%"
+            />
+          </div>
+          <div className="Three">
+            {" "}
+            <img
+              src={restaurantPicture2}
+              alt="restaurantPicture"
+              width="100%"
+              height="100%"
+            />
+          </div>
         </div>
 
         <div className="ChefSection">
@@ -65,14 +101,44 @@ const Home = () => {
             <div className="NamesTitle">
               <h2 className="Team">Our Team</h2>
             </div>
-            <div className="ChefNamesList">hi</div>
+            <div className="ChefNamesList">
+              <p className="ChefDesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum varius nisl nec purus luctus dictum. Cras dapibus
+                metus eu lacinia commodo. Aenean vehicula erat turpis.
+                Suspendisse sed imperdiet leo. Vestibulum consequat blandit
+                euismod. Aenean at lacinia libero, in molestie mi. Maecenas ac
+                turpis. Curabitur a tincidunt tortor. Cras at dui pellentesque,
+                varius sapien id, porta mauris. Sed tempus dui ut justo aliquam,
+                vel
+              </p>
+              <button className="ChefButton">
+                More <FaArrowRight />{" "}
+              </button>
+            </div>
           </div>
           <div className="ChefVIP">
             <Sliderr images={images1} />
           </div>
         </div>
       </div>
-      <div className="Reviews">hi</div>
+      <div className="Reviews">
+        <h2 className="Team">Our Food is Delicious</h2>
+        <p className="Paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          varius nisl nec purus luctus dictum. Cras dapibus metus eu lacinia
+          commodo. Aenean vehicula erat turpis. Suspendisse sed imperdiet leo.
+          Vestibulum consequat blandit euismod. Aenean at lacinia libero, in
+          molestie mi. Maecenas ac diam tincidunt tortor consectetur elementum
+          non ac metus. Duis sollicitudin dui vitae aliquet convallis. Mauris a
+          viverra turpis.
+        </p>
+
+        <div className="Buttons">
+          <button className="ButtonMenu">Menus</button>
+          <button className="ButtonReserve">Reserve</button>
+        </div>
+      </div>
     </div>
   );
 };
