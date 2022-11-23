a0fa805a5dd742b8c677ddc62c3a9c80f67cbbda
IsabelNzioka <isabelnzioka@gmail.com>
2022-11-23 23:21:45 +0300

    landingpage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import Home from "./components/Home/Home";
 
 import Gallery from "./components/Gallery/Gallery";
 import Menu from "./components/Menu/Menu";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 import "./App.css";
 
 const App = () => {
   const routes = (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/gallery" element={<Gallery />} /> */}
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/gallery" element={<Gallery />} />
       <Route path="/menu/*" element={<Menu />} />
     </Routes>
   );
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div className="One"></div>
+      <div className="One"></div>
+      <div className="One"></div>
+      <div className="One"></div>
+      <div className="One"></div>
+      <div className="One"></div>
+    </div>
+  );
+};
+
+export default LandingPage;
