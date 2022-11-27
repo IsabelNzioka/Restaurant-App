f7f595386344fa32b8be23a2368aa19acbbcc33c
IsabelNzioka <isabelnzioka@gmail.com>
2022-11-27 05:23:22 +0300

    landingpage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -7,6 +7,8 @@ import chef1 from "../../assets/img/chef/chef2.jpg";
 import chef4 from "../../assets/img/chef/chef5.jpg";
 import restaurantPicture1 from "../../assets/img/rest17.jpg";
 import restaurantPicture2 from "../../assets/img/rest16.jpg";
+import foodImage1 from "../../assets/img/food/food1.jpg";
+import foodImage2 from "../../assets/img/food/food10.jpg";
 
 import "./LandingPage.css";
 import "../Home/Home.css";
@@ -16,36 +18,20 @@ const LandingPage = () => {
     <div className="LandingPage">
       <div className="SectionOne">Hello</div>
       <div className="HomeContent">
-        <h2 className="Icon">{/* <FaTablets /> */}</h2>{" "}
-        <FaYelp className="Iconn" />
+        {/* <h2 className="Icon"></h2> */}
+        {/* <FaYelp className="Iconn" /> */}
         <h2 className="Welcome">Welcome</h2>
-        <p>
-          {" "}
+        <p className="WelcomeContent">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
           varius nisl nec purus luctus dictum. Cras dapibus metus eu lacinia
           commodo. Aenean vehicula erat turpis. Suspendisse sed imperdiet leo.
           Vestibulum consequat blandit euismod. Aenean at lacinia libero, in
-          molestie mi. Maecenas ac diam tincidunt tortor consectetur elementum
-          non ac metus. Duis sollicitudin dui vitae aliquet convallis. Mauris a
-          viverra turpis. Curabitur a tincidunt tortor. Cras at dui
-          pellentesque, varius sapien id, porta mauris. Sed tempus dui ut justo
-          aliquam, vel
+          molestie mi.
         </p>
-        <FaYelp className="Iconnn" />
-        {/* <h3>About Us</h3> */}
+        {/* <FaYelp className="Iconnn" /> */}
       </div>
-      <hr />
-
+      {/* <hr /> */}
       <div className="RestaurantSection">
-        {/* <div className="On">
-          <img
-            src={restaurantPicture1}
-            alt="restaurantPicture"
-            width="100%"
-            height="100%"
-          />
-        </div> */}
-
         <div className="Restaurant">
           <div className="One">
             <div className="OneOne">
@@ -90,62 +76,20 @@ const LandingPage = () => {
 
         {/* ///////////////// */}
         {/* <div className="On">
-          <img
-            src={restaurantPicture2}
-            alt="restaurantPicture"
-            width="100%"
-            height="100%"
-          />
-        </div>
-        <div className="On">
-          <img src={chef4} alt="restaurantPicture" width="100%" height="100%" />
-        </div> */}
+            <img
+                src={restaurantPicture2}
+                alt="restaurantPicture"
+                width="100%"
+                height="100%"
+            />
+            </div>
+            <div className="On">
+            <img src={chef4} alt="restaurantPicture" width="100%" height="100%" />
+            </div> */}
       </div>
-      <hr />
 
-      <div className="RestaurantSection">
-        <div className="Restaurant">
-          <div className="One">
-            <div className="OneOne">
-              <h2 className="RestaurantName">
-                La Belle <span>Restaurant</span>
-              </h2>
-            </div>
-            <div className="OneTwo">
-              <p className="RestaurantDesc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum varius nisl nec purus luctus dictum. Cras dapibus
-                metus eu lacinia commodo. Vestibulum consequat blandit euismod.
-                Aenean at lacinia libero, in molestie mi. Maecenas ac turpis.
-                Curabitur a tincidunt tortor. Cras at dui pellentesque, varius
-                sapien id, porta mauris. Sed tempus dui ut justo aliquam, vel
-              </p>
-
-              <button className="RestaurantButton">
-                More <FaArrowRight />{" "}
-              </button>
-            </div>
-          </div>
-
-          <div className="Two">
-            <img
-              src={restaurantPicture1}
-              alt="restaurantPicture"
-              width="100%"
-              height="100%"
-            />
-          </div>
-          <div className="Three">
-            {" "}
-            <img
-              src={restaurantPicture2}
-              alt="restaurantPicture"
-              width="100%"
-              height="100%"
-            />
-          </div>
-        </div>
-        {/* <div className="On">
+      <div className="MenuSectionPage">
+        <div className="MenuPageImage">
           <img
             src={restaurantPicture1}
             alt="restaurantPicture"
@@ -153,18 +97,48 @@ const LandingPage = () => {
             height="100%"
           />
         </div>
-        <div className="On">
+        <div className="MenuPageContent">
+          <h2 className="Team">Happenings</h2>
+          <p className="RestaurantDesc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            varius nisl nec purus luctus dictum. Cras dapibus metus eu lacinia
+            commodo. Vestibulum consequat blandit euismod. Aenean at lacinia
+            libero, in molestie mi. Maecenas ac turpis.{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className="ChefSection">
+        <div className="RestOne">
           <img
-            src={restaurantPicture2}
+            src={restaurantPicture1}
             alt="restaurantPicture"
             width="100%"
             height="100%"
           />
         </div>
-        <div className="On">
-          <img src={chef4} alt="restaurantPicture" width="100%" height="100%" />
-        </div> */}
+        <div className="RestTwo">
+          <div className="RestTwo1">
+            <img
+              src={foodImage1}
+              alt="restaurantPicture"
+              width="100%"
+              height="100%"
+            />
+          </div>
+          <div className="RestTwo2">
+            {" "}
+            <img
+              src={foodImage2}
+              alt="restaurantPicture"
+              width="100%"
+              height="100%"
+            />
+          </div>
+        </div>
       </div>
+
+      <div className="SectionLast">One</div>
     </div>
   );
 };
