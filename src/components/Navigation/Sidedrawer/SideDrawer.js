480fd992977b05c9825c2ba4407a1b492f9e3707
IsabelNzioka <isabelnzioka@gmail.com>
2022-12-11 02:16:50 +0300

    sidebar

--- src/components/Navigation/Sidedrawer/SideDrawer.js
+++ src/components/Navigation/Sidedrawer/SideDrawer.js
@@ -3,6 +3,15 @@ import React from "react";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 
+import { FaYelp } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+
 import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
@@ -20,6 +29,41 @@ const SideDrawer = (props) => {
         <nav>
           <NavigationItems />
         </nav>
+        <hr />
+
+        <div className="Location">
+          <p>
+            <FaMapMarkerAlt /> karen bay shore, Nairobi, Kenya
+          </p>
+          <p>
+            <FaPhoneAlt /> +254746282937
+          </p>
+          <p>
+            <FaPhoneAlt /> +254783493629
+          </p>
+        </div>
+        <div className="Socialss">
+          <p>
+            {" "}
+            <FaInstagram />
+          </p>
+          <p>
+            {" "}
+            <FaTwitter />
+          </p>
+          <p>
+            {" "}
+            <FaFacebook />
+          </p>
+          <p>
+            {" "}
+            <FaFacebook />
+          </p>
+          <p>
+            {" "}
+            <FaFacebook />
+          </p>
+        </div>
       </div>
     </div>
   );
